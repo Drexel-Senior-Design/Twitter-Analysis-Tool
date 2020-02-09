e9fb86e9f3e860c7f2a4c8b5c2de904082a6d9be
rushw <39307793+rushweigelt@users.noreply.github.com>
2020-02-09 03:47:39 -0500

    More Django implementation

--- Django/TwitterAnalysisTool/tat/models.py
+++ Django/TwitterAnalysisTool/tat/models.py
@@ -2,6 +2,7 @@
 Rush Weigelt
 Feb 2020
 This script is to create Django objects out of our ML models
+Lots of helper functions
 '''
 
 #Django imports
@@ -11,20 +12,20 @@ from sklearn.naive_bayes import GaussianNB, BernoulliNB
 from sklearn.model_selection import cross_validate, train_test_split, KFold
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
-#import seaborn as sb
 from sklearn import metrics
 from pymongo import MongoClient
 import numpy as np
 from joblib import load
-
+#from tensorflow.python.keras.models import load_model, model_from_json
+from keras.utils import CustomObjectScope
 
 #Hardcoded function to open our client and look at a specific collection to train our model
+#Returns a Pandas Dataframe Object
 def read_mongo_data(database, collection):
     client = MongoClient('bvm15.cci.drexel.edu')
     db = client[database]
     df = db[collection]
     df = pd.DataFrame(list(df.find()))
-    #print(test)
     return df
     #This did not work, and did not seem neccesary for the amt of data we have
     #Create query to the specific DB and Collection
@@ -37,8 +38,8 @@ def read_mongo_data(database, collection):
         #del df['_id']
     #return df
 
-#Helper function to be used after read_mongo_data
-#Fill nas, convert all columns from strings to numberic
+#Helper function to be used after read_mongo_data: fill nas, convert all columns from strings to numberic
+#Returns two pandas dataframe, one a df of our features, the other of our labels
 def clean_data_strings_to_ints(dataframe):
     dataframe.fillna(0, inplace=True)
     fieldnames_list = ['followerscount', 'friendscount', 'replycount', 'likecount', 'retweetcount', 'hashtagcount',
@@ -59,6 +60,7 @@ def clean_data_strings_to_ints(dataframe):
 
     return x, y_df
 
+#This cleans the data we get from twitter for numerical analysis.
 def clean_twitter_data_strings_to_ints(dataframe):
     dataframe.fillna(0, inplace=True)
     fieldnames_list = ['favoritecount', 'quotecount', 'replycount', 'retweetcount']
@@ -69,17 +71,20 @@ def clean_twitter_data_strings_to_ints(dataframe):
     # Replace NaNs with 0s for use in ML
     x = x.replace(np.nan, 0, regex=True)
     x = x.rename(columns={'favoritecount': 'likecount'})
-    #y = dataframe['label']
-    # Convert labels to a list, then convert labels into ints
-    # Bots = 0, Genuine = 1
-    #y_int = y.values.tolist()
-    #y_int = [int(0) if x == 'bot' else int(1) for x in y_int]
-    #y_df = pd.DataFrame(y_int)
-
     return x
+
+def clean_twitter_data_text_analysis(dataframe):
+    dataframe.fillna(0, inplace=True)
+    fieldnames_list = ['text']
+    x = dataframe['text']
+    x = x.replace(np.nan, 0, regex=True)
+    return x
+
 
 # Create your models here.
 '''
+EXAMPLE FOR NEW DJANGO PEOPLE: THIS DID NOT WORK OUT BECAUSE CLASSES RUN AT STARTUP
+FUNCTIONS ARE BETTER TO WRITE FOR ML MODELS, AS THEY WONT ALWAYS BE CALLED
 class GaussianNB():
     data = read_mongo_data()
     x, y_df = clean_data_strings_to_ints(data)
@@ -99,23 +104,47 @@ class GaussianNB():
 def LoadModel(mfn):
     if mfn == 'gaussianNB':
         model = load('tat/mlModels/gaussianNB.joblib')
+    elif mfn == 'LSTMText':
+        model = load('tat/mlModels/LSTMModel.joblib')
     return model
 
+#Function for Gaussian-based Naive Bayes numerical analysis
 def GaussianNB(db, collect):
+    #Load requested data from database
     data = read_mongo_data(db, collect)
     data = clean_twitter_data_strings_to_ints(data)
-    #print(data)
-    #print(data)
-    m = LoadModel('gaussianNB')
     x = data.values.tolist()
-    #print(x)
-    #for entry in x:
-     #   print(m.predict([entry]))
+    #load our model and predict on our new data
+    m = LoadModel('gaussianNB')
     predictions = m.predict(x)
     print(predictions)
+    #Light number crunching for report
     bot_num = np.sum(predictions=='bot')
     percent = bot_num/len(predictions)*100
     statement = "Out of {} analyzed tweets, {} are suspected bots. That is {}%!".format(len(predictions), bot_num, round(percent,2))
+    print(statement)
+    return statement
+
+#Function for our LSTM Textual Classifier
+def LSTMTextClassifier(db, collect):
+    #Load requested data from database
+    data = read_mongo_data(db, collect)
+    data = clean_twitter_data_text_analysis(data)
+    x = data.values.tolist()
+    #load model and predict
+    '''
+    m = LoadModel('LSTMText')
+    predictions - m.predict(x)
+    '''
+    #load model and weights, then open them and predict on new data
+    model = 'mlModels/LSTMModel.json'
+    weights = 'mlModel/LStmwiegelts_mobilenet.h5'
+    with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
+        with open(model, 'r') as f:
+            model = model_from_json(f.read())
+        model.load_weights(weights)
+    predictions = model.predict(x)
+    statement = "Out of {} analyzed tweets, {} are suspected bots. That is {}%!".format(len(predictions), bot_num, round(percent, 2))
     print(statement)
     return statement
 
--- Django/TwitterAnalysisTool/tat/views.py
+++ Django/TwitterAnalysisTool/tat/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.core.exceptions import *
 
-from tat.models import GaussianNB
+from tat.models import GaussianNB, LSTMTextClassifier
 
 # Create your views here.
 def index(request):
@@ -26,7 +26,12 @@ def about(request):
 def search(request):
     if request.method == 'POST':
         user_hashtag = request.POST.get('user_hashtag', None)
-        html = GaussianNB('Hashtags', user_hashtag)
+        user_model = request.POST.get('user_model', None)
+        #print(user_model)
+        if user_model == 'nb':
+            html = GaussianNB('Hashtags', user_hashtag)
+        else:
+            html = LSTMTextClassifier('Hashtags', user_hashtag)
         print(user_hashtag)
         return HttpResponse(html)
         #return render(request, 'tat/search.html', user_hashtag)
