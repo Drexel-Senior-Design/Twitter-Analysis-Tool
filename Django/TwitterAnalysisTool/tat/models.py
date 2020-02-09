77071645f9b4b943927833eafe9cdc5cf8f41779
rushw <39307793+rushweigelt@users.noreply.github.com>
2020-02-09 15:10:16 -0500

    Both model types work
    Django implementation works for both sklearn models and keras/tensorflow models.
    
    This means adding more models will be as simple as making them, saving them, and following the implemented template on how to load them.

--- Django/TwitterAnalysisTool/tat/models.py
+++ Django/TwitterAnalysisTool/tat/models.py
@@ -16,8 +16,14 @@ from sklearn import metrics
 from pymongo import MongoClient
 import numpy as np
 from joblib import load
+from tensorflow.python.keras.initializers import glorot_uniform
+from tensorflow.python.keras.models import load_model, model_from_json
 #from tensorflow.python.keras.models import load_model, model_from_json
 from keras.utils import CustomObjectScope
+import re
+import nltk
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 
 #Hardcoded function to open our client and look at a specific collection to train our model
 #Returns a Pandas Dataframe Object
@@ -73,12 +79,74 @@ def clean_twitter_data_strings_to_ints(dataframe):
     x = x.rename(columns={'favoritecount': 'likecount'})
     return x
 
-def clean_twitter_data_text_analysis(dataframe):
+def clean_twitter_data_text_analysis(df):
+    '''
     dataframe.fillna(0, inplace=True)
     fieldnames_list = ['text']
     x = dataframe['text']
-    x = x.replace(np.nan, 0, regex=True)
     return x
+    '''
+    #Clean data exactly as we do before training model
+    df.fillna(0, inplace=True)
+    df = df.applymap(str)
+    #labels = df['label'].map(lambda x: 1 if x == 'bot' else 0)
+    df = df[df.text.apply(lambda x: x != "")]
+    #df = df[df.description.apply(lambda x: x != "")]
+
+    # print(df.describe())
+    # print(df.head)
+    ##TAKEN FROM @sabbar
+    def clean_text(text):
+        # Convert words to lower case and split them
+        text = text.lower().split()
+        # Remove stop words
+        # stops = set(stopwords.words("english"))
+        text = " ".join(text)
+        text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
+        text = re.sub(r"what's", "what is ", text)
+        text = re.sub(r"\'s", " ", text)
+        text = re.sub(r"\'ve", " have ", text)
+        text = re.sub(r"n't", " not ", text)
+        text = re.sub(r"i'm", "i am ", text)
+        text = re.sub(r"\'re", " are ", text)
+        text = re.sub(r"\'d", " would ", text)
+        text = re.sub(r"\'ll", " will ", text)
+        text = re.sub(r",", " ", text)
+        text = re.sub(r"\.", " ", text)
+        text = re.sub(r"!", " ! ", text)
+        text = re.sub(r"\/", " ", text)
+        text = re.sub(r"\^", " ^ ", text)
+        text = re.sub(r"\+", " + ", text)
+        text = re.sub(r"\-", " - ", text)
+        text = re.sub(r"\=", " = ", text)
+        text = re.sub(r"'", " ", text)
+        text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
+        text = re.sub(r":", " : ", text)
+        text = re.sub(r" e g ", " eg ", text)
+        text = re.sub(r" b g ", " bg ", text)
+        text = re.sub(r" u s ", " american ", text)
+        text = re.sub(r"\0s", "0", text)
+        text = re.sub(r" 9 11 ", "911", text)
+        text = re.sub(r"e - mail", "email", text)
+        text = re.sub(r"j k", "jk", text)
+        text = re.sub(r"\s{2,}", " ", text)
+        ## Stemming
+        text = text.split()
+        stemmer = nltk.SnowballStemmer('english')
+        stemmed_words = [stemmer.stem(word) for word in text]
+        text = " ".join(stemmed_words)
+        return text
+
+    ##end taken from @sabbar
+    # apply clean text to our tweet data
+    df['text'] = df['text'].map(lambda x: clean_text(x))
+    # limit vocab size, then tokenize as a preprocessing step
+    vocab_size = 20000
+    tokenizer = Tokenizer(num_words=vocab_size)
+    tokenizer.fit_on_texts(df['text'])
+    sequences = tokenizer.texts_to_sequences(df['text'])
+    data = pad_sequences(sequences, maxlen=50)
+    return data
 
 
 # Create your models here.
@@ -130,21 +198,46 @@ def LSTMTextClassifier(db, collect):
     #Load requested data from database
     data = read_mongo_data(db, collect)
     data = clean_twitter_data_text_analysis(data)
-    x = data.values.tolist()
+    #x = data.values.tolist()
     #load model and predict
-    '''
-    m = LoadModel('LSTMText')
-    predictions - m.predict(x)
-    '''
+
+    #m = LoadModel('LSTMText')
+    #predictions - m.predict(x)
+
     #load model and weights, then open them and predict on new data
-    model = 'mlModels/LSTMModel.json'
-    weights = 'mlModel/LStmwiegelts_mobilenet.h5'
+
+    model = 'tat/mlModels/LSTMModel.json'
+    weights = 'tat/mlModels/LSTMweights.h5'
     with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
         with open(model, 'r') as f:
             model = model_from_json(f.read())
         model.load_weights(weights)
-    predictions = model.predict(x)
-    statement = "Out of {} analyzed tweets, {} are suspected bots. That is {}%!".format(len(predictions), bot_num, round(percent, 2))
+    preds = model.predict_classes(data, verbose=2)
+    probs = model.predict_proba(data, verbose=2)
+    preds_arry = np.array(preds)
+    probs_arry = np.array(probs)
+    # Bots = 0, Genuine = 1
+    combined = pd.DataFrame(np.hstack((preds_arry,probs_arry)))
+    preds_lst = combined[0].tolist()
+    probs_lst =  combined[1].tolist()
+    #bots = 0, genuine = 1
+    for x in preds_lst:
+        if x ==1.0:
+            preds_lst[preds_lst.index(x)] = 'genuine'
+        else:
+            preds_lst[preds_lst.index(x)] ='bot'
+    #print(preds_lst)
+
+    #new_preds = ['bot' if x == [0] else 'genuine' for x in [preds_lst]]
+    #print(probs_lst)
+    #print(new_preds)
+    i = 0
+    for x in preds_lst:
+        print("Prediction: {}       Probability: {}".format(x, probs_lst[i]))
+        i + 1
+    bot_num = preds_lst.count('bot')
+    percent = bot_num/len(preds_lst)*100
+    statement = "Out of {} analyzed tweets, {} are suspected bots. That is {}%!".format(len(preds_lst), bot_num, round(percent, 2))
     print(statement)
     return statement
 
