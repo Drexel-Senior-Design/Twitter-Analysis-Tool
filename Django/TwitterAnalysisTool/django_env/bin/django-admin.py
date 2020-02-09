3586c8c1462147999f2fd90be6b5583e8cf5cb36
rushw <39307793+rushweigelt@users.noreply.github.com>
2020-02-09 17:46:56 -0500

    Try 3 to upload enviornment properly

---
+++ Django/TwitterAnalysisTool/django_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/RushWeigelt/Documents/GitHub/Twitter-Analysis-Tool/Django/TwitterAnalysisTool/django_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
