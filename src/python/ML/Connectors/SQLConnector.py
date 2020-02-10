1ec852fe6aae54298df35d61fdf27f1d15969c60
ljv27 <ljv27@drexel.edu>
2020-02-10 09:00:30 -0500

    first commit of SQLConnector, scrap validation.py, incorporate __init__.py imports from changes in dead branch

---
+++ src/python/ML/Connectors/SQLConnector.py
@@ -0,0 +1,54 @@
+from collections import Counter
+from random import shuffle
+
+import mysql.connector
+from mysql.connector import errorcode
+
+from ML.Connectors import Connector
+
+
+class SQLConnector(Connector):
+    def __init__(self, **kwargs):
+        self.connection = mysql.connector.connect(**kwargs)
+        self.cursor = self.connection.cursor()
+    
+    def retrieve(self, table, fields, test_size=None, train_size=None, batch_size=None):
+        if not batch_size:
+            userid_query = f"SELECT userid FROM {table}"
+            self.cursor.execute(userid_query)
+            userids = Counter(userid for (userid,) in self.cursor)
+            
+            if test_size is None and train_size is None:
+                test_size = 0.25
+                train_size = 1 - test_size
+            elif train_size is None:
+                train_size = 1 - test_size
+            elif test_size is None:
+                test_size = 1 - train_size
+            else:
+                raise ValueError('Test and train sizes must be floats that add to 1.')
+
+            num_tweets = sum(userids.values())
+            proportions = [(userid, count / num_tweets) for (userid, count) in userids.items()]
+            shuffle(proportions)
+
+            train_users = []
+            train_total = 0
+            for (userid, proportion) in proportions:
+                if train_total < train_size:
+                    train_users.append(userid)
+                    train_total = train_total + proportion
+
+            tweet_query = f"SELECT {', '.join([userid, *fields])} FROM {table}"
+            self.cursor.execute(tweet_query)
+
+            train_set = []
+            test_set = []
+            for result in self.cursor:
+                userid = result[0]
+                if userid in train_users:
+                    train_set.append(result[1:])
+                else:
+                    test_set.append(result[1:])
+
+            return train_set, test_set
---
+++ src/python/ML/Connectors/__init__.py
@@ -0,0 +1,3 @@
+from ML.Connectors.Connector import Connector
+from ML.Connectors.MockConnector import MockConnector
+from ML.Connectors.SQLConnector import SQLConnector
