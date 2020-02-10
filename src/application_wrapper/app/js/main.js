f67a29d15cb0e6f43471c4ad5d2d441ce7b02f22
Chris Bakopoulos <christian.bakopoulos@gmail.com>
2020-02-10 13:08:39 -0500

    Cleaned up some frontend elements, added Twitter API connection and ability to search for hashtags.

--- src/application_wrapper/app/js/main.js
+++ src/application_wrapper/app/js/main.js
@@ -1,8 +1,47 @@
 /*
+Event Listeners
+ */
+document.addEventListener("DOMContentLoaded", function(){
+	var hashtag_grab_btn = document.getElementById("hashtag_btn");
+	hashtag_grab_btn.onclick = Get_Tweets_in_Hashtag;
+	if (T) {
+		document.getElementById("tweet_connection_status").innerHTML = "Yep";
+	}
+});
+
+/*
 variables
 */
-var fakeDataX = [4, 2, 5, 20, 20, 1, 5, 3]
+var fakeDataX = [4, 2, 5, 20, 20, 1, 5, 3];
 
+// Establish Twitter Connection
+let Twit = require("twit");
+var T = new Twit({
+	consumer_key:         '',
+	consumer_secret:      '',
+	access_token:         '',
+	access_token_secret:  '',
+});
+
+function Get_Tweets_in_Hashtag(){
+	let hashtag = document.getElementById("user_hashtag").value;
+	// TODO: Better input validation on Hashtag
+	if (hashtag.length > 140 || hashtag.search("#") !== -1){
+		alert("Bad input, too long or invalid characters");
+		throw "Get_Tweets_in_Hashtag(): Input Too long or Invalid Chars";
+	}
+	T.get('search/tweets',
+		{ q: '#'+hashtag, count: 100 },
+		function(err, data, response) {
+			// TODO: Error Handling
+			if (!err) {
+				let tweet = data.statuses[0];
+				document.getElementById("user").innerHTML = tweet.user.name;
+				document.getElementById("tweetbody").innerHTML = tweet.text;
+			}
+		}
+	);
+}
 
 /*
 scrape hashtag that the user enters
@@ -10,10 +49,4 @@ scrape hashtag that the user enters
 function Get_User_Hashtag() {
 	var x = document.getElementById("user_hashtag").value;
 	document.getElementById("report_display").innerHTML = x;
-}
-
-function run_NB() {
-	if hash node 2/dev/null; then
-		node NaiveBayes_Gaussian.js 4 2 5 20 20 1 5 3
-	fi
 }
--- src/application_wrapper/main.js
+++ src/application_wrapper/main.js
@@ -3,6 +3,11 @@
 let { app, BrowserWindow } = require('electron');
 
 app.on('ready', function() {
-    let mainWindow = new BrowserWindow();
+    let mainWindow = new BrowserWindow({
+        webPrefrences: {
+            nodeIntegration : true
+    }});
+    mainWindow.setMenu(null);
     mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+    mainWindow.webContents.openDevTools()
 });
