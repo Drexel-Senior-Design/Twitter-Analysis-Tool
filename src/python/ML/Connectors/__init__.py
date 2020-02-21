fdb80f26ab8ef1b4487b62c6ebfa14d5c1439e0c
vitalos <ljv27@drexel.edu>
2020-02-21 15:06:40 -0500

    fileconnector

--- src/python/ML/Connectors/__init__.py
+++ src/python/ML/Connectors/__init__.py
@@ -1,3 +1,4 @@
 from ML.Connectors.Connector import Connector
 from ML.Connectors.MockConnector import MockConnector
 from ML.Connectors.SQLConnector import SQLConnector
+from ML.Connectors.FileConnector import FileConnector
